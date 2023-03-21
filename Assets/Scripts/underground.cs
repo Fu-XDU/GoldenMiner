7e742189271a2fae7940877350f8d19336b7d4e0
付铭 <fuming.137@icloud.com>
2023-03-21 23:36:22 +0800

    update

--- Assets/Scripts/underground.cs
+++ Assets/Scripts/underground.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using System;
 public class underground : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+String o = GameObject.Find("Canvas/time").GetComponent<Text>().text;
+        GameObject.Find("Canvas/time").GetComponent<Text>().text = "12";
+
+		//time.text = "12";
+
     }
 
     // Update is called once per frame
